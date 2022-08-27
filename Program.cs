db5ba1433dc547cd723ac8d74daca8a1a92545dd
underdogg <47274328+und3rd0gg@users.noreply.github.com>
2022-08-27 18:41:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Weapon
+{
+    private uint _bullets;
+
+    public bool CanShoot() => _bullets > 0;
+
+    public void Shoot()
+    {
+        if (CanShoot())
+            _bullets -= 1;
+    }
+}
